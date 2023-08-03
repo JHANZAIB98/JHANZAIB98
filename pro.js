4231b1a260ea5ff444e18a48962a0bfb253ed75e
jhanzaib98 <prjahanzaib1@gmail.com>
2023-08-03 00:17:33 -0700

    just added new items

--- pro.js
+++ pro.js
@@ -1,4 +1,32 @@
 let search = document.querySelector('.search');
-document.querySelector('#search').onclick=()=>{
+document.querySelector('#search').onclick = () => {
     search.classList.toggle('active');
 }
+// const swiperEl = new swiper(".product-row")
+// Object.assign(swiperEl, {
+//   centerSlides: true,
+//   spaceBetween: 30,
+//   autoplay:{
+//     delay:9500,
+//     disableOnInteraction: false,
+//   }
+//   pagination: {
+//     el:".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 1,
+      
+//     },
+//     768: {
+//       slidesPerView: 2,
+      
+//     },
+//     1024: {
+//       slidesPerView: 3,
+      
+//     },
+//   },
+// });
+// swiperEl.initialize();
